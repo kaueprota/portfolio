753fac53030fbb61d185a08e6ba8805614b35195
Kauê Prota <kaueprota18@gmail.com>
2022-12-13 22:09:23 -0300

    V0.9

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-var menu_click = document.querySelector(".img_menu_2")
-var menu_twoclick = document.querySelector(".img_menu")
-var nav = document.querySelector("#nav")
-menu_click.addEventListener('click', ()=>{   
-nav.style.display = "flex"
-})
 
-menu_twoclick.addEventListener('click', () =>{
-    nav.style.display = "none"
-})
+class Menu{
+    menuMobile(){
+        let menu_click = document.querySelector(".img_menu_2")
+        let menu_mobile = document.querySelector(".menu_mobile")
+        let menu_twoclick = document.querySelector(".img_menu")
+        menu_click.addEventListener('click', ()=>{   
+            menu_mobile.classList.add("menu_mobile_toggle")
+            menu_click.style.display = "none"
+        })
+
+        menu_twoclick.addEventListener('click',()=>{
+            menu_mobile.classList.remove("menu_mobile_toggle")
+            menu_click.style.display = "inline"
+        })
+        
+        
+    }
+}
+
+let menu = new Menu()
+menu.menuMobile()
+
